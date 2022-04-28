83c644d5c28821ec2bf8863412c17bd02ca4405a
preeti-max <77143565+preeti-max@users.noreply.github.com>
2022-04-28 21:51:42 +0530

    Update SavingsAccountDao.java

--- src/main/java/com/hendisantika/onlinebanking/dao/SavingsAccountDao.java
+++ src/main/java/com/hendisantika/onlinebanking/dao/SavingsAccountDao.java
@@ -3,16 +3,7 @@ package com.hendisantika.onlinebanking.dao;
 import com.hendisantika.onlinebanking.model.SavingsAccount;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : online-banking
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 08/08/18
- * Time: 06.07
- * To change this template use File | Settings | File Templates.
- */
+
 public interface SavingsAccountDao extends CrudRepository<SavingsAccount, Long> {
 
     SavingsAccount findByAccountNumber(int accountNumber);
