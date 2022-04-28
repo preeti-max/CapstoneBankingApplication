cb8b1d4fc3c8b5d186163da59a8add580d769c35
preeti-max <77143565+preeti-max@users.noreply.github.com>
2022-04-28 21:50:38 +0530

    Update PrimaryAccountDao.java

--- src/main/java/com/hendisantika/onlinebanking/dao/PrimaryAccountDao.java
+++ src/main/java/com/hendisantika/onlinebanking/dao/PrimaryAccountDao.java
@@ -3,16 +3,7 @@ package com.hendisantika.onlinebanking.dao;
 import com.hendisantika.onlinebanking.model.PrimaryAccount;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : online-banking
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 08/08/18
- * Time: 06.04
- * To change this template use File | Settings | File Templates.
- */
+
 public interface PrimaryAccountDao extends CrudRepository<PrimaryAccount, Long> {
 
     PrimaryAccount findByAccountNumber(int accountNumber);
